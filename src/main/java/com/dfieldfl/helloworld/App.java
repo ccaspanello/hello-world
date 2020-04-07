6147b8aa8268240ef69099b2447e218dfa57210b
Brandon Groves <brandon.groves@gmail.com>
2020-04-06 21:51:38 -0400

    asdf

--- src/main/java/com/dfieldfl/helloworld/App.java
+++ src/main/java/com/dfieldfl/helloworld/App.java
@@ -11,8 +11,8 @@ public class App
         System.out.println( "Hello World!" );
 
         for(int i = 0; i < 100; i++) {
-          Thread.sleep(10000);
           try {
+            Thread.sleep(10000);
             throw new NullPointerException("whoops");
           } catch(Exception e) {
             //swallow
